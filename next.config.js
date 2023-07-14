2b99b2a92dd8e350c6de59b6cb77d9de96cb397e
kelvin <kelvinchengjiajun@gmail.com>
2023-07-15 05:40:22 +0800

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     APIURL: "https://localhost:7028/api",
-    AUTHURL: "https://localhost:7028/auth"
+    AUTHURL: "https://localhost:7028/auth",    
+    BING_COOKIE:"1DmUDupUND3Z0BIcsaWK4NUgbmdjwY4c6ipUUWN1Cxf_tRO4wiHdmjpoaw-FhSgJQsitlpm-CHdQZPbFdxL2nMgWlmqYnTYrA_YqBBtZXFrZkOJ-p-gDMNkJ9ky08i0fsUnkTsvMpB0mRflM1MWXeZBuyny2Hx1JF34VnosLpy_puh6dCIUpcue3bE971a4JeGQppbX5gTGMNJI6qXZXgG02TbTZoPpMgcRUmGazRnLQ"
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
