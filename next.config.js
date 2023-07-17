74f1332e44f4324b00625b0d3e39780386a9ab5f
Kelvin <2001427@sit.singaporetech.edu.sg>
2023-07-18 04:32:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     APIURL: "https://invify.azurewebsites.net/api",
     AUTHURL: "https://invify.azurewebsites.net/auth",  
-    BING_COOKIE:"1vDNof-eXcKR7kuyvliL66fBZHPiQvxWUNNIYgJIOhZjOXrG0aYonQ-E8OFGCEuGEgY3SH4MWYTVNhznrM4L6Qe-vxrQO1b1VTO6t1Xyyr8NOxl4EKbFfJOZ_uihTHYDM1vkX9UeqZc227TZBOFTCPSUfEjTWvKyhGvGaH2IENPfApjmiegFAbl5MHZTEYASOe0QGF0CQN03RdZNr4HuGWwb6yjP41anTWnh-x7VtIdM"
+    BING_COOKIE:"1DmUDupUND3Z0BIcsaWK4NUgbmdjwY4c6ipUUWN1Cxf_tRO4wiHdmjpoaw-FhSgJQsitlpm-CHdQZPbFdxL2nMgWlmqYnTYrA_YqBBtZXFrZkOJ-p-gDMNkJ9ky08i0fsUnkTsvMpB0mRflM1MWXeZBuyny2Hx1JF34VnosLpy_puh6dCIUpcue3bE971a4JeGQppbX5gTGMNJI6qXZXgG02TbTZoPpMgcRUmGazRnLQ"
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
