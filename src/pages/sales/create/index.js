127551ae8f83d260aeaf8634ccfb83c924cac8a2
kelvin <kelvinchengjiajun@gmail.com>
2023-07-19 01:10:56 +0800

    modified:   src/pages/sales/create/index.js

--- src/pages/sales/create/index.js
+++ src/pages/sales/create/index.js
@@ -82,8 +82,7 @@ const CreateSales = () => {
     price: Yup.string().test(
       "is-decimal",
       "Price must be a decimal number with two decimal places",
-      (value) => (value+"").match(/^\d*(\.\d{2})$/)
-      
+      (value) => (value + "").match(/^\d*(\.\d{2})$/)
     ),
   });
 
@@ -108,10 +107,7 @@ const CreateSales = () => {
         .then((res) => {
           //console.log(res);
           if (res.status == 200) {
-            openSnackbar(
-              "Sales created successfully",
-              "success"
-            );
+            openSnackbar("Sales created successfully", "success");
             formik.resetForm();
           } else {
             openSnackbar(res.data.message, "error");
@@ -129,136 +125,137 @@ const CreateSales = () => {
 
   return (
     <Layout>
-        <form onSubmit={formik.handleSubmit}>
-          <BGrid>
-            <Grid item xs={12}>
-              <h1 className="text-xl text-darkaccent-100 font-semibold overflow-ellipsis">
-                Create Sale
-              </h1>
-            </Grid>
-            <div className="justify-between space-y-4">
-              <Autocomplete
-                open={open}
-                color="light"
-                onOpen={() => {
-                  setOpen(true);
-                }}
-                onClose={() => {
-                  setOpen(false);
-                }}
-                isOptionEqualToValue={(option, value) =>
-                  option.name === value.name
-                }
-                getOptionLabel={(option) => option.name}
-                options={options}
-                loading={loading}
-                onChange={(event, value) => {
-                  formik.setFieldValue("productId", value.id);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    className=" bg-darkaccent-800 rounded-lg"
-                    required
-                    variant="filled"
-                    color="light"
-                    label="Product"
-                    InputLabelProps={{
-                      className: "text-darkaccent-100",
-                    }}
-                    InputProps={{
-                      ...params.InputProps,
-                      className: "text-darkaccent-100",
-                      endAdornment: (
-                        <React.Fragment>
-                          {loading ? (
-                            <CircularProgress color="inherit" size={20} />
-                          ) : null}
-                          {params.InputProps.endAdornment}
-                        </React.Fragment>
-                      ),
-                    }}
-                  />
-                )}
-              />
-              <TextField
-                required
-                fullWidth
-                id="quantity"
-                name="quantity"
-                label="Quantity"
-                placeholder="e.g. 10"
-                variant="filled"
-                margin="normal"
-                color="light"
-                InputLabelProps={{
-                  className: "text-darkaccent-100",
-                }}
-                InputProps={{
-                  className: "text-darkaccent-100",
-                }}
-                className=" bg-darkaccent-800 rounded-lg"
-                value={formik.values.quantity}
-                onChange={formik.handleChange}
-                error={
-                  formik.touched.quantity && Boolean(formik.errors.quantity)
-                }
-                helperText={formik.touched.quantity && formik.errors.quantity}
-              />
-              <TextField
-                required
-                fullWidth
-                id="price"
-                name="price"
-                label="Price"
-                placeholder="e.g. 1000.00"
-                variant="filled"
-                margin="normal"
-                color="light"
-                InputLabelProps={{
-                  className: "text-darkaccent-100",
-                }}
-                InputProps={{
-                  className: "text-darkaccent-100",
-                }}
-                className=" bg-darkaccent-800 rounded-lg"
-                value={formik.values.price}
-                onChange={formik.handleChange}
-                error={formik.touched.price && Boolean(formik.errors.price)}
-                helperText={formik.touched.price && formik.errors.price}
-              />
-              <DatePicker
-                // defaultValue={dayjs(Date.now())}
-                defaultValue={dayjs(Date.now()).format('YYYY-MM-DDTHH:mm:ss')}
-
-                className=" rounded-lg"
-                label="Sale Date"
-                slotProps={{
-                  field: { className:"text-darkaccent-100"},
-                  textField: { variant:"filled", color:"light", className:" text-darkaccent-100 bg-darkaccent-800 rounded-lg" },
-                }}
-                value = {selectedDate}
-                //value={dayjs(selectedDate)}
-                //value={dayjs(selectedDate)}
-                onChange={(date) => {
-                  handleDateChange(dayjs(date))                   
-                  formik.setFieldValue("saleDate", dayjs(date).format('YYYY-MM-DDTHH:mm:ss'));
-                
-                }}
-                
-              />
-
-              <Button
-                type="submit"
-                className="text-white bg-darkshade-400 font-semibold lowercase p-2 rounded-lg"
-                disabled={isSubmitting}
-              >
-                Submit
-              </Button>
-              </div>
-          </BGrid>
-        </form>
-        
+      <form onSubmit={formik.handleSubmit}>
+        <BGrid>
+          <Grid item xs={12}>
+            <h1 className="text-xl text-darkaccent-100 font-semibold overflow-ellipsis">
+              Create Sale
+            </h1>
+          </Grid>
+          <Grid xs={6}>
+            <Autocomplete
+              open={open}
+              color="light"
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              isOptionEqualToValue={(option, value) =>
+                option.name === value.name
+              }
+              getOptionLabel={(option) => option.name}
+              options={options}
+              loading={loading}
+              onChange={(event, value) => {
+                formik.setFieldValue("productId", value.id);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  className=" bg-darkaccent-800 rounded-lg"
+                  required
+                  variant="filled"
+                  color="light"
+                  label="Product"
+                  InputLabelProps={{
+                    className: "text-darkaccent-100",
+                  }}
+                  InputProps={{
+                    ...params.InputProps,
+                    className: "text-darkaccent-100",
+                    endAdornment: (
+                      <React.Fragment>
+                        {loading ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+            <TextField
+              required
+              fullWidth
+              id="quantity"
+              name="quantity"
+              label="Quantity"
+              placeholder="e.g. 10"
+              variant="filled"
+              margin="normal"
+              color="light"
+              InputLabelProps={{
+                className: "text-darkaccent-100",
+              }}
+              InputProps={{
+                className: "text-darkaccent-100",
+              }}
+              className=" bg-darkaccent-800 rounded-lg"
+              value={formik.values.quantity}
+              onChange={formik.handleChange}
+              error={formik.touched.quantity && Boolean(formik.errors.quantity)}
+              helperText={formik.touched.quantity && formik.errors.quantity}
+            />
+            <TextField
+              required
+              fullWidth
+              id="price"
+              name="price"
+              label="Price"
+              placeholder="e.g. 1000.00"
+              variant="filled"
+              margin="normal"
+              color="light"
+              InputLabelProps={{
+                className: "text-darkaccent-100",
+              }}
+              InputProps={{
+                className: "text-darkaccent-100",
+              }}
+              className=" bg-darkaccent-800 rounded-lg"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
+            />
+            <DatePicker
+              // defaultValue={dayjs(Date.now())}
+              defaultValue={dayjs(Date.now()).format("YYYY-MM-DDTHH:mm:ss")}
+              className=" rounded-lg"
+              label="Sale Date"
+              slotProps={{
+                field: { className: "text-darkaccent-100" },
+                textField: {
+                  variant: "filled",
+                  color: "light",
+                  className:
+                    " text-darkaccent-100 bg-darkaccent-800 rounded-lg",
+                },
+              }}
+              value={selectedDate}
+              onChange={(date) => {
+                handleDateChange(dayjs(date));
+                formik.setFieldValue(
+                  "saleDate",
+                  dayjs(date).format("YYYY-MM-DDTHH:mm:ss")
+                );
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              className="text-white bg-darkshade-400 font-semibold lowercase p-2 rounded-lg"
+              disabled={isSubmitting}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </BGrid>
+      </form>
     </Layout>
   );
 };
