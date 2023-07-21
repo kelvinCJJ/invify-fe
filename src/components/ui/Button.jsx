91431d3aa42c732a05acd2dbdada4a693000dbba
kelvin <kelvinchengjiajun@gmail.com>
2023-07-22 01:24:01 +0800

    modified:   src/components/ui/Button.jsx

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { ButtonBase, Button as MuiButton } from '@mui/material';
 
-const Button = ({ severity, children, ...props }) => {
-    let className = 'text-md  md:text-base rounded-md normal-case p-2 lg:px-4'
+const Button = ({ severity, children , ...props }) => {
+    const [className, setClassName] = useState('text-md  md:text-base rounded-md normal-case p-2 lg:px-4');
+
+    useEffect(() => { 
+      let className1 = 'text-md  md:text-base rounded-md normal-case p-2 lg:px-4'
+
     switch (severity) {
       case 'error':
-        className += ' text-white bg-error-500 hover:bg-error-800';
+        className1 += ' text-white bg-error-500 hover:bg-error-800';
+        setClassName(className1);
         break;
       case 'warning':
-        className += ' text-white bg-warning-500 hover:bg-warning-800';
+        className1 += ' text-white bg-warning-500 hover:bg-warning-800';
+        setClassName(className1);
         break;
       case 'info':
-        className += ' text-white bg-info-500 hover:bg-info-800 ';
+        className1 += ' text-white bg-info-500 hover:bg-info-800 ';
+        setClassName(className1);
         break;
       case 'success':
-        className += ' text-white bg-success-500 hover:bg-success-800';
+        className1 += ' text-white bg-success-500 hover:bg-success-800';
+        setClassName(className1);
         break;
       case 'icon':
-        className += 'text-white bg-transparent  hover:bg-darkshade-400';
+        className1 += 'text-white bg-transparent  hover:bg-darkshade-400';
+        setClassName(className1);
         break;
       default:
-        className += ' text-primary-900 bg-lightshade-500 hover:bg-lightshade-600';
+        className1 += ' text-primary-900 bg-lightshade-500 hover:bg-lightshade-600';
+        setClassName(className1);
         break;
     }
+  }, [severity]);
   
     return (
-      <ButtonBase {...props} className={className} >
+      <ButtonBase  className={className}  {...props}>
         {children}
       </ButtonBase>
     );
